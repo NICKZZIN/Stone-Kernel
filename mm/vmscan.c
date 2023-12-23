1e81f27e3319a1ce773252e9887d802b0255826e
Yu Zhao <yuzhao@google.com>
2023-12-22 21:56:47 -0700

    mm/mglru: skip special VMAs in lru_gen_look_around()
    Special VMAs like VM_PFNMAP can contain anon pages from COW.  There isn't
    much profit in doing lookaround on them.  Besides, they can trigger the
    pte_special() warning in get_pte_pfn().
    
    Skip them in lru_gen_look_around().
    
    Link: https://lkml.kernel.org/r/20231223045647.1566043-1-yuzhao@google.com
    Fixes: 018ee47f1489 ("mm: multi-gen LRU: exploit locality in rmap")
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Reported-by: syzbot+03fd9b3f71641f0ebf2d@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/000000000000f9ff00060d14c256@google.com/
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4151,6 +4151,7 @@ void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 	struct lru_gen_mm_walk *walk;
 	int young = 0;
 	unsigned long bitmap[BITS_TO_LONGS(MIN_LRU_BATCH)] = {};
+	struct vm_area_struct *vma = pvmw->vma;
 	struct page *page = pvmw->page;
 	bool can_swap = !page_is_file_cache(page);
 	struct mem_cgroup *memcg = page_memcg(page);
@@ -4165,11 +4166,15 @@ void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 	if (spin_is_contended(pvmw->ptl))
 		return;
 
+	/* exclude special VMAs containing anon pages from COW */
+	if (vma->vm_flags & VM_SPECIAL)
+		return;
+
 	/* avoid taking the LRU lock under the PTL when possible */
 	walk = current->reclaim_state ? current->reclaim_state->mm_walk : NULL;
 
-	start = max(pvmw->address & PMD_MASK, pvmw->vma->vm_start);
-	end = min(pvmw->address | ~PMD_MASK, pvmw->vma->vm_end - 1) + 1;
+	start = max(pvmw->address & PMD_MASK, vma->vm_start);
+	end = min(pvmw->address | ~PMD_MASK, vma->vm_end - 1) + 1;
 
 	if (end - start > MIN_LRU_BATCH * PAGE_SIZE) {
 		if (pvmw->address - start < MIN_LRU_BATCH * PAGE_SIZE / 2)
@@ -4190,7 +4195,7 @@ void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 	for (i = 0, addr = start; addr != end; i++, addr += PAGE_SIZE) {
 		unsigned long pfn;
 
-		pfn = get_pte_pfn(pte[i], pvmw->vma, addr);
+		pfn = get_pte_pfn(pte[i], vma, addr);
 		if (pfn == -1)
 			continue;
 
@@ -4201,7 +4206,7 @@ void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 		if (!page)
 			continue;
 
-		if (!ptep_test_and_clear_young(pvmw->vma, addr, pte + i))
+		if (!ptep_test_and_clear_young(vma, addr, pte + i))
 			VM_WARN_ON_ONCE(true);
 
 		young++;
